73c5ccaa186dcb3b35a580bca33fe03f8c70702a
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2022-10-25 15:03:35 +0200

    Create WHERE CLAUSE.sql

---
+++ W3SchoolTutorials/DML/WHERE CLAUSE.sql
@@ -0,0 +1,7 @@
+SELECT * FROM first_table
+WHERE country='Mexico'
+
+/*result:
+customerid  customername                        contactname     address                       city        postalcode country
+2	          Ana Trujillo Emparedados y helados	Ana Trujillo	  Avda. de la Constitucion 2222	Mexico D.F.	05021	     Mexico
+3	          Antonio Moreno Taquería	            Antonio Moreno	Mataderos 2312	              México D.F.	05023	     Mexico*/

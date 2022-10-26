8dc94e4c285c477efc2112c2ea82770f4d3cbe3d
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2022-10-26 08:12:14 +0200

    Create ErzeugeTabelleErsteNormalform.sql

---
+++ Normalformen/ErzeugeTabelleErsteNormalform.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS erstenf_table;
+
+ CREATE Table erstenf_table(
+  
+ ISBN int, /* zusammengesetzter (ganzer) Primärschlüssel*/
+ Kundennummer int,
+ AutorNachname varchar(255),
+ AutorVorname varchar(255),
+ Titel varchar(255),
+ Menge int,
+ PRIMARY KEY (ISBN, Kundennummer)
+ );
+
+ INSERT INTO erstenf_table (ISBN, Kundennummer, AutorNachname, AutorVorname, Titel, Menge)
+ 
+ VALUES ('343', '1', 'Schwinn', 'Hans', 'Relationale DB', '5'),
+        ('344', '15', 'Schwinn', 'Hans', 'Relationale DB', '6'),
+        ('345', '16', 'Hughes', 'John', 'Objektorientiert DB', '2')
+       
